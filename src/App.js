0b1b99dfd1317b63a6e4568ae0198c7a176c997a
Melissa Dagley <melissadagley@gmail.com>
2022-02-06 15:04:05 -0600

    Add some filler content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+
+const Article = () => {
+  return (
+    <div className='article'>
+      <h2>Article Title</h2>
+      <p>A 2006 New York Times article outlined the economics of keeping pandas, which costs five times more than keeping the next most expensive animal, an elephant. American zoos generally pay the Chinese government $1 million a year in fees, as part of a typical ten-year contract. San Diego's contract with China was to expire in 2008, but got a five-year extension at about half of the previous yearly cost. The last contract, with the Memphis Zoo in Memphis, Tennessee, ended in 2013.</p>
+      <a>Read More...</a>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='title'>
+        <h1>The Panda Post</h1>
+      </div>
+      <div id='articleGrid'>
+        <Article />
+        <Article />
+        <Article />
+        <Article />
+        <Article />
+        <Article />
+      </div>
     </div>
   );
 }
