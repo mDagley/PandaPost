ae9915ba9b7718fe2bc365b201f18b69ef8d30b5
Melissa Dagley <melissadagley@gmail.com>
2022-02-06 17:03:14 -0600

    Call news api to populate article feed

--- src/App.js
+++ src/App.js
@@ -1,29 +1,65 @@
 import './App.css';
+import React, {useEffect, useState } from 'react';
+import axios from 'axios';
 
-const Article = () => {
+
+const Article = (article) => {
+  
   return (
     <div className='article'>
-      <h2>Article Title</h2>
-      <p>A 2006 New York Times article outlined the economics of keeping pandas, which costs five times more than keeping the next most expensive animal, an elephant. American zoos generally pay the Chinese government $1 million a year in fees, as part of a typical ten-year contract. San Diego's contract with China was to expire in 2008, but got a five-year extension at about half of the previous yearly cost. The last contract, with the Memphis Zoo in Memphis, Tennessee, ended in 2013.</p>
+      <img className='articleImage' src={article.urlToImage} />
+      <h2>{article.title}</h2>
+      <p>{article.description}</p>
       <a>Read More...</a>
     </div>
   );
 }
 
+const ArticleGrid = (props) => {
+  if(props.articles != undefined && props.articles != ''){
+    console.log('ArticleGrid ', props.articles)
+    return (
+      <div id='articleGrid'>
+        {props.articles.map(article => <Article key={article.title} {...article} />)}
+      </div>
+    )
+  }
+
+  else {
+    return (<h3>Loading...</h3>)
+  }
+  
+}
+
 function App() {
+  let [articles, setArticles] = useState('');
+
+  const getArticles = React.useCallback(() => {
+    axios({
+      'method': 'GET',
+      'url': 'https://newsapi.org/v2/everything?q=%22giant%20panda%22&searchIn=title&from=2022-02-05&to=2019-02-05&sortBy=popularity',
+      'headers': {
+        'X-API-Key': process.env.REACT_APP_NEWS_API_KEY
+      }
+    })
+    .then((response) => {
+      setArticles(response.data.articles)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }, []); 
+  
+  useEffect(() => {
+    getArticles()
+  }, [getArticles])
+
   return (
     <div className="App">
       <div className='title'>
         <h1>The Panda Post</h1>
       </div>
-      <div id='articleGrid'>
-        <Article />
-        <Article />
-        <Article />
-        <Article />
-        <Article />
-        <Article />
-      </div>
+      <ArticleGrid articles = {articles}/>
     </div>
   );
 }
